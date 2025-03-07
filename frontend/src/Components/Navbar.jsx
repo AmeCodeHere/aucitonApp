c17485f8cae5757c7cd03d1199490f435754a276
AmeCodeHere <kt1740594@gmail.com>
2025-03-07 14:55:55 +0530

    working on ui

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -6,9 +6,29 @@ const Navbar = () => {
 
         <div className='container'>
 
-            <div className='nav-items'>
+            <div className='nav-logo'>
+                <p className='p1'>
+                    A
+                </p>
+                <div>
 
-                <div className='left-items'>
+                    <span className='s1'>Auction</span>
+                    <span className='s2'>Here</span>
+                </div>
+            </div>
+
+            <div className="contains-items">
+                <div className='user'>
+                    <Link className='link' to="/"  ><p>Home</p></Link>
+                    <Link className='link' to="/"  ><p>Dashboard</p></Link>
+                    <Link className='link' to="/auctionPage">  <p>Add Auction</p></Link>
+                </div>
+                <div className="btns">
+                    <button className='btn1'><p>Log in</p></button>
+                    <button className='btn2'>Get started for free</button>
+                </div>
+            </div>
+            {/* <div className='left-items'>
                   <Link className='link' to="/">  <span>Dashboard</span></Link> 
                   <Link className='link' to="/auctionPage">  <span>Post Auction</span></Link> 
                  
@@ -23,9 +43,9 @@ const Navbar = () => {
                 <Link className='link' to="/login">  <span>Login</span></Link> 
                 <Link className='link' to="/signup">  <span>SignUp</span></Link> 
            
-                </div>
+                </div> */}
 
-            </div>
+
         </div>
 
     )
